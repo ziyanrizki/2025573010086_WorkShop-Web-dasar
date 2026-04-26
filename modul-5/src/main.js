e663c2f20c0c3361a09243fac54f002950423b50
ziyan elio <ziyanriski9@gmail.com>
2026-04-26 12:43:38 +0700

    bagian-4-komponen UI & dark mode

--- modul-5/src/main.js
+++ modul-5/src/main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+const btn = document.getElementById('toggleDark')
+const html = document.documentElement
+if (localStorage.getItem('theme') === 'dark') {
+    html.classList.add('dark')
+    btn.textContent = 'light'
+}
+btn.addEventListener('click', () => {
+    html.classList.toggle('dark')
+    const isDark = html.classList.contains('dark')
+    btn.textContent = isDark ? 'light' : 'dark'
+    localStorage.setItem('theme', isDark ? 'dark' : 'light')
+})
